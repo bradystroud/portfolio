1e61a92d1d5da4cd3598ac018d6dded41ed0d1b3
Brady Stroud <bradystroud@ssw.com.au>
2022-03-13 07:37:31 +1000

    📈 Test AppInsights

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using BlazorApplicationInsights;
 using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,5 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddMatBlazor();
+builder.Services.AddBlazorApplicationInsights();
+
 
 await builder.Build().RunAsync();
