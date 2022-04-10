e8b414d87ec9676003bd962ae3c67efffd51d741
Brady Stroud <bradystroud@ssw.com.au>
2022-04-10 11:51:35 +1000

    Improvements

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using BlazorApplicationInsights;
-using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
@@ -9,9 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped(_ => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddScoped<IContentService, ContentService>();
-builder.Services.AddMatBlazor();
 builder.Services.AddBlazorApplicationInsights();
 
 
