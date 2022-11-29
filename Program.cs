5651ec71467a793cfa0f3e037adb312d825d4624
Brady Stroud <bradystroud@ssw.com.au>
2022-11-29 18:59:11 +1000

    Clean up home page

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Portfolio;
 using Portfolio.Interfaces;
 using Portfolio.Services;
@@ -11,5 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddScoped<IProjectsService, ProjectsService>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
